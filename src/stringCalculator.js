762dc8a7ccd18991880ab6a8a93d70de83de6728
Anshul Maher <anshulmaher016@gmail.com>
2025-08-30 21:12:11 +0530

    Implemented custom single-character delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,22 @@
+function escapeRegExp(s) {
+  return s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
 function stringCalculator(input) {
   if (input === "") return 0;
 
-  const numbers = input.split(/,|\n/).map(Number);
+  let delimiters = /,|\n/;
+  let numbersString = input;
+
+  if (input.startsWith("//")) {
+    const match = input.match(/^\/\/(.)\n(.*)/);
+    if (match) {
+      delimiters = new RegExp(escapeRegExp(match[1]));
+      numbersString = match[2];
+    }
+  }
+
+  const numbers = numbersString.split(delimiters).map(Number);
   return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
