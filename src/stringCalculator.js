e59e37937a9b175d8d54341f5c573b6e2a3ec42d
Anshul Maher <anshulmaher016@gmail.com>
2025-08-30 21:02:35 +0530

    Implemented empty string, single number, and two numbers sum

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,8 @@
 function stringCalculator(input) {
-  return input;
+  if (input === "") return 0;
+
+  const numbers = input.split(",").map(Number);
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
 module.exports = stringCalculator;
