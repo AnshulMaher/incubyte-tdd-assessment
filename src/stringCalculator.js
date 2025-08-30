620389ea23d1aedd6b5dfbfa4b823c1c3c9b491e
Anshul Maher <anshulmaher016@gmail.com>
2025-08-30 22:48:29 +0530

    refactored code

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,35 +1,41 @@
-function escapeRegExp(s) {
-  return s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
+const escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
-function stringCalculator(input) {
-  if (input === "") return 0;
-
-  let delimiters = /,|\n/;
-  let numbersString = input;
-
-  if (input.startsWith("//")) {
-    const match = input.match(/^\/\/(.*?)\n(.*)/s);
-    if (match) {
-      delimiters = match[1];
-      if (delimiters.startsWith("[") && delimiters.endsWith("]")) {
-        delimiters = [...delimiters.matchAll(/\[([^\]]+)\]/g)]
-          .map((m) => escapeRegExp(m[1]))
-          .join("|");
-      }
-      delimiters = new RegExp(delimiters);
-      numbersString = match[2];
-    }
+const getDelimitersRegex = (match) => {
+  if (!match) {
+    return /,|\n/;
   }
 
-  const numbers = numbersString.split(delimiters).map(Number);
+  let delimiterPart = match[1];
+  if (delimiterPart.startsWith("[") && delimiterPart.endsWith("]")) {
+    delimiterPart = [...delimiterPart.matchAll(/\[([^\]]+)\]/g)]
+      .map((m) => escapeRegExp(m[1]))
+      .join("|");
+  }
 
-  const negatives = numbers.filter((n) => n < 0);
-  if (negatives.length > 0) {
+  return new RegExp(delimiterPart);
+};
+
+const stringCalculator = (input) => {
+  if (input === "") return 0;
+
+  const match = input.match(/^\/\/(.*?)\n(.*)/s);
+  const delimiters = getDelimitersRegex(match);
+  const numbersString = match ? match[2] : input;
+
+  const negatives = [];
+  let sum = 0;
+  let numberStrs = numbersString.split(delimiters);
+  for (const numStr of numberStrs) {
+    const n = Number(numStr);
+    if (n < 0) negatives.push(n);
+    else if (n <= 1000) sum += n;
+  }
+
+  if (negatives.length) {
     throw new Error(`Negatives not allowed: ${negatives.join(", ")}`);
   }
 
-  return numbers.reduce((acc, curr) => (curr <= 1000 ? acc + curr : acc), 0);
-}
+  return sum;
+};
 
 module.exports = stringCalculator;
