109ac86fe34f51061fca5c81972cc9fd2f27b96e
Anshul Maher <anshulmaher016@gmail.com>
2025-08-30 21:04:26 +0530

    Added test for sum of unknown amount of numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -12,4 +12,8 @@ describe("StringCalculator", () => {
   test("returns sum of two numbers separated by comma", () => {
     expect(stringCalculator("1,2")).toBe(3);
   });
+
+  test("sums unknown number of numbers", () => {
+    expect(stringCalculator("1,2,3,4,5")).toBe(15);
+  });
 });
